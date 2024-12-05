3c00e580025087c8eeb4be00dc596d262d51f0b9
Parmesh Kumar <76766381+Parmeshkumardogra@users.noreply.github.com>
2024-12-05 13:53:12 +0530

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -7,7 +7,7 @@ const Experience = () => {
         <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">Experience</p>
         <div className="cursor-pointer mt-10 mx-auto max-w-lg p-8 rounded-lg hover:mb-[-20px] hover:scale-110 hover:border-y-white hover:border-y-8 hover:shadow-xl duration-300">
           <h2 className="text-2xl font-bold mb-4">Jio Platforms Limited</h2>
-          <h3 className="text-lg font-bold mb-4">Graduate Engineer Trainee <br/>(Software Developer)</h3>
+          <h3 className="text-lg font-bold mb-4">Software Developement Engineer 1</h3>
           <p className="text-base leading-relaxed">
             Currently, I am actively involved in a project where I am utilizing my expertise in JavaScript, Node.js, Express.js, and MongoDB to develop robust and scalable web applications. Additionally, I am also expanding my knowledge by exploring Docker, Kubernetes, and Kafka and also interested in cloud-based technologies like AWS and Azure. I have a keen interest in deployment and messaging systems.
           </p>
